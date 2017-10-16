10e43f9ee81a5751a98af85bc45d68cc46ba3e32
sailaja1234 <sailaja.pasumarthy@gmail.com>
2017-10-16 16:55:08 +0530

    role based access code of django application

---
+++ adobepro/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from .views import (
+    login_view,
+)
+from .views import logout_view, register_view
+
+urlpatterns = [
+    url(r'^login/$', login_view, name = "login"),
+]
+urlpatterns += [
+    url(r'^logout/$', logout_view, name = "logout"),
+]
+
+urlpatterns += [
+    url(r"^register/$", register_view, name = "register"),
+]
